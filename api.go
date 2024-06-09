56baa2fbd2134a82e0721a18013e65227ba1ead5
Roli Christen <roland.christen@jaywalker.ch>
2024-06-09 11:40:24 +0200

    added util functions

--- api.go
+++ api.go
@@ -1,8 +1,10 @@
 package configcove
 
 import (
+	"github.com/rollicks-c/configcove/internal/fsio"
 	"github.com/rollicks-c/configcove/profiles"
 	"github.com/rollicks-c/configcove/store"
+	"os"
 )
 
 func Profiles[T any](appName string) *profiles.Manager[T] {
@@ -12,4 +14,16 @@ func Profiles[T any](appName string) *profiles.Manager[T] {
 
 func Store(appName string) *store.Manager {
 	return store.New(appName)
+}
+
+func HomeDir() string {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	return homeDir
+}
+
+func ConfigDir(appName string) string {
+	return fsio.GetConfigDir(appName)
 }
